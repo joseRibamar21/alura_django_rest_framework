6a14e7916bf0100e803eb9b0d7760b3d34671f20
José Ribamar Pilar de Araújo <jose.21rb@outlook.com>
2023-07-20 14:00:37 -0300

    feat: serializador

--- escola/admin.py
+++ escola/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from escola.models import Aluno, Curso
 
-# Register your models here.
+class Alunos(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'rg', 'cpf', 'data_nascimento') # Campos que serão exibidos na listagem
+    list_display_links = ('id', 'nome') # Campos que serão exibidos como links para alterar
+    search_fields = ('nome',) # Campos que serão pesquisados
+    list_per_page = 20 # Quantidade de registros por página
+
+admin.site.register(Aluno, Alunos) # Registra o modelo Aluno e a Classe admin do mesmo
+
+class Cursos(admin.ModelAdmin):
+    list_display = ('id', 'codigo_curso', 'descricao', 'nivel') 
+    list_display_links = ('id', 'codigo_curso') 
+    search_fields = ('codigo_curso',) 
+    list_per_page = 20 
+
+admin.site.register(Curso, Cursos) 
--- escola/views.py
+++ escola/views.py
@@ -1,7 +1,13 @@
-from django.http import JsonResponse
+from rest_framework import viewsets
+from escola.models import Aluno, Curso
+from serializer import AlunosSerializer, CursosSerializer
 
-# Create your views here.
-def alunos(request):
-    if request.method == 'GET':
-        aluno = {'id':1, 'nome':'João', 'idade': 20}
-        return JsonResponse(aluno)
+class AlunosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os alunos e alunas"""
+    queryset = Aluno.objects.all()
+    serializer_class = AlunosSerializer
+
+class CursosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os cursos"""
+    queryset = Curso.objects.all()
+    serializer_class = CursosSerializer
